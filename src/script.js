b217225b72c6a613287d28136a2388a754fdb5ca
Xaren <xaren@Xarens-MacBook-Pro.local>
2023-08-04 23:56:12 -0600

    changed db to script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,29 @@
 // Mostrar mockClients
+const clients = [
+    { 
+        id: '12378710', 
+        name: 'Margo', 
+        lastname: 'Zavala', 
+        birthday: '9/DIC/1993', 
+        password: 'hi', 
+        saldo: 900 
+    }, 
+    { 
+        id: '12368794',
+        name: 'Frank', 
+        lastname: 'Grimes', 
+        birthday: '04/MAY/1997', 
+        password: 'password', 
+        saldo: 15 
+    }, 
+    { 
+        id: '12345678',
+        name: 'Hank', 
+        lastname: 'Scorpio', 
+        birthday: '3/NOV/1996' ,  
+        password: '', 
+        saldo: 1200 
+    }]
 
 clients.forEach(client => {
     console.log('user: ' + client.id + ', password: ' + client.password);
